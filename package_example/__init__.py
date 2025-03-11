d455469ad9b2058bb48f3463dd8c656f17555152
Martin V. Larsen <martivl@stud.ntnu.no>
2025-03-11 14:04:27 +0100

    fix

--- package_example/__init__.py
+++ package_example/__init__.py
@@ -1,11 +1,6 @@
 # package_example/__init__.py
-# package_example/__init__.py
-import sys
-import os
+from .module import hello, no
 
-# Go one directory up to include big_project in Python’s search path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from ttt import no
 
 __version__ = "0.1.0"
